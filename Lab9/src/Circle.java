c513d9a007d70250c0d1cdfeb65933a621d107ff
hokka1do <kyzya.illia@gmail.com>
2019-09-05 21:21:38 +0300

    Done

---
+++ Lab9/src/Circle.java
@@ -0,0 +1,25 @@
+public class Circle {
+    private static int radius = 20;
+    private static String color;
+
+    static double circleLength(int radius) {
+        double result = 2 * radius * Math.PI;
+        return result;
+    }
+
+    static double circleSquare(int radius) {
+        double result = Math.PI * Math.pow(radius, 2);
+        return result;
+    }
+
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, int radius) {
+        this.color = color;
+        this.radius = radius;
+    }
+
+}
