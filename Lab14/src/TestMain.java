7cf586f0259d1b7605a28728936c57c17fae5b7d
hokka1do <kyzya.illia@gmail.com>
2019-09-18 16:01:57 +0300

    Done

--- Lab14/src/TestMain.java
+++ Lab14/src/TestMain.java
@@ -1,8 +1,4 @@
 import Shop.Human.Customer.Customer;
-import Shop.Human.Employee.Cashier.Cashier;
-import Shop.Human.Employee.Employee;
-import Shop.Human.Employee.Manager.Manager;
-import Shop.Human.Employee.Security.Security;
 import Shop.Shop;
 
 public class TestMain {
@@ -11,14 +7,13 @@ public class TestMain {
         //1)
         System.out.println("Case no1 ---->");
         newShop.open();
-        Employee.comeToWork();
         System.out.println();
         //2)
         System.out.println("Case no2 ---->");
         newShop.saleProducts();
         Customer.askAboutProduct();
         System.out.println();
-        //3 Was modified "go for lunch" calling from shop method. So we just call "go for lunch" once from shop.
+        //3
         System.out.println("Case no3 ---->");
         newShop.goForLunch();
     }
